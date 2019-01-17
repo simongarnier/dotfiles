45c0620dff237fe9600e0bc5f0d64d3f34942387
Simon Garnier <simgarni@amazon.com>
2019-01-16 16:04:18 -0800

    .vimrc

---
+++ .vimrc
@@ -0,0 +1,4 @@
+syntax on
+filetype plugin indent on
+set tabstop=4
+set expandtab
--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-[ -f ~/.zshrc ] && cp ~/.zshrc ~/.zshrc.old
-cp ./.zshrc ~/.zshrc
+function install_dotfile(){
+    [ -f ~/$1 ] && cp ~/$1 ~/$1.old
+    cp ./$1 ~/$1
+}
+
+# zsh
+install_dotfile .zshrc
 touch ~/.zsh_profile
+
+# vim
+install_dotfile .vimrc
